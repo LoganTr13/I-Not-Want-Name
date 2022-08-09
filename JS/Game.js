90d92e8cf3fe2b0f40c165a56d382e070773cefb
LoganTr13 <logaxgamer@gmail.com>
2022-08-09 20:55:39 -0300

    Segundo Commit

--- JS/Game.js
+++ JS/Game.js
@@ -1,18 +1,39 @@
 var Scenery = document.getElementById("GScreen");
 var x = Scenery.style.backgroundPositionX;
 var y = Scenery.style.backgroundPositionY;
-
+var speed = 10;
+Scenery.style.backgroundPositionY = "-200px";
+Scenery.style.backgroundPositionX = "120px";
+console.log(Scenery.style.backgroundPosition);
 
 document.onkeydown = Player;
 function Player(e)
 {
-    if(e.Code = "KeyW")
+    if(e.keyCode == 87)
     {
-
-        x += 
-        Scenery.style.backgroundPositionX = toString(x) + "px";
+        y = parseInt(Scenery.style.backgroundPositionY);
+        y += speed;
+        Scenery.style.backgroundPositionY = y + "px";
     }
-    
+    if(e.keyCode == 83)
+    {
+        y = parseInt(Scenery.style.backgroundPositionY);
+        y -= speed;
+        Scenery.style.backgroundPositionY = y + "px";
+    }
+    if(e.keyCode == 65)
+    {
+        x = parseInt(Scenery.style.backgroundPositionX);
+        x += speed;
+        Scenery.style.backgroundPositionX = x + "px";
+    }
+    if(e.keyCode == 68)
+    {
+        x = parseInt(Scenery.style.backgroundPositionX);
+        x -= speed;
+        Scenery.style.backgroundPositionX = x + "px";
+    }
+
 }
 
 function OnRenderFrame()
---
+++ ReadMe.md
@@ -0,0 +1,8 @@
+***
+# I Not Want Name
+
+ É um jogo para eu aprender mais a programar, desfrutando do basico do desenvolvimento de WEB, no jogo você terá que coleta itens para conseguir pontos.
+
+ O jogo é feito para receber criticas para que eu possa aprender mais e assim evoluir meus conhecimentos, o jogo usa imagens aleatorias da net, ja que foi projetado para aprendizagem, e não comercio, mas caso aconteça algo sobre direito autoral, me avise que irei remover do jogo o mais rapido possivel, obrigado.
+
+ ***
